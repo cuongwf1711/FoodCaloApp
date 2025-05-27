046cf61508de95faeec1d9827d8ad2edc2c21b5e
cuongwf1711 <ruivalien@gmail.com>
2025-05-27 21:41:46 +0700

    ok

--- constants/url_constants.ts
+++ constants/url_constants.ts
@@ -4,7 +4,7 @@ export const BASE_URL = 'https://mighty-baboon-busy.ngrok-free.app/api/v1';
 
 // npx expo export --platform web
 // eas deploy --alias v1
-// eas build--platform android--profile development
+// eas build --platform android --profile development
 
 export default BASE_URL;
 
